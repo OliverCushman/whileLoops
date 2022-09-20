7c12eaabf311166e8d4db2c2f85bef0d23c308e1
Oliver Cushman <oliver.cushman@mrjaffesclass.com>
2022-09-20 11:45:05 -0700

    inital commit

---
+++ whileLoops.java
@@ -0,0 +1,36 @@
+public class whileLoops
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    int i = 0;
+    int k = 20;
+    
+    public void run() {
+        while (i < 15) {
+            System.out.println("i is" + i + "but is less than 15");
+            i++;
+        }
+        while (k > 5) {
+            System.out.println("k is" + k + "but is greater than 5");
+            k--;
+        }
+        while (a < 10 && b <= 20) {
+            System.out.println("a is" + a + "but is less than 10 and b is"
+            + b + "but is less than or equal to 20");
+            a++;
+            if (b < 20) {
+                b++;
+            }
+        }
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        whileLoops wl = new whileLoops();
+        wl.run();
+    }
+}
